7444f3a3fa902296dfc7df6531f131d9a858e8b2
unknown <184571@BME.HU>
2024-11-08 13:25:33 +0100

    IMSc

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,52 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void store_Is_Empty() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+        // Act
+        boolean result = store.isEmpty();
+
+        // Assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    void store_Is_Not_Empty() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act
+        boolean result = store.isEmpty();
+
+        // Assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    void store_Count_Success() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(2);
+
+        // Act
+        store.fire(1);
+
+        // Assert
+        assertEquals(1, store.getTorpedoCount());
+    }
+
+    @Test
+    void fire_Failure() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+        // Act
+        
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> { store.fire(1); });
     }
 }
